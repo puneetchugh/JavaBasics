62845ddeacf73b3a10f7af56072e6d2f9bf5cbfe
Puneet Chugh <chughpuneet2005@gmail.com>
2019-03-11 12:51:38 -0600

    Multithreading : Safe critical section

---
+++ MultiThreading/SafeCriticalSection/src/com/puneet/chugh/SafeCriticalSection.java
@@ -0,0 +1,57 @@
+package com.puneet.chugh;
+import java.lang.Runnable;
+import java.lang.Thread;
+
+public class SafeCriticalSection{
+
+	int counter1 = 0;
+	int counter2 = 0;
+	Integer counterLock1  = new Integer(1);
+	Integer counterLock2  = new Integer(2);
+
+	public void add(int val1, int val2){
+		synchronized(this){
+			counter1 = counter1 + val1;
+			counter2 = counter2 + val2;
+		}
+	}
+
+	public void printResult(){
+		System.out.printf(String.format("counter1 = %d and counter2 = %d\n",counter1, counter2));
+	}	
+
+	public static class MyRunnable implements Runnable{
+		
+		SafeCriticalSection critical;
+		int val1 = 0;
+		int val2 = 0;
+		public MyRunnable(SafeCriticalSection critical, int val1, int val2){
+			this.critical = critical;
+			this.val1 = val1;
+			this.val2 = val2;
+		}
+		
+		@Override
+		public void run(){
+			critical.add(val1, val2);
+		} 
+	}
+	
+	public static void main(String[] args){
+		
+		SafeCriticalSection critical = new SafeCriticalSection();
+		critical.printResult();
+		Thread thread1 = new Thread(new MyRunnable(critical, 10, 15));
+		thread1.start();
+		try{
+			thread1.join();
+		}catch(InterruptedException excep){}
+		critical.printResult();
+		Thread thread2 = new Thread(new MyRunnable(critical, 10, 15));
+		thread2.start();
+		try{
+			thread2.join();
+		}catch(InterruptedException excep){}
+		critical.printResult();
+	}
+}
