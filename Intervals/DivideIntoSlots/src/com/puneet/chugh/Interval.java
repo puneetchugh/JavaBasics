9a517b06ccca3f6f0bab8d1f75288489fcb8f84f
Puneet Chugh <chughpuneet2005@gmail.com>
2019-04-24 17:06:57 -0600

    Adding Interval class

---
+++ Intervals/DivideIntoSlots/src/com/puneet/chugh/Interval.java
@@ -0,0 +1,12 @@
+package com.puneet.chugh;
+
+public class Interval{
+	
+	int start, end;
+	
+	public Interval(){}
+	public Interval(int start, int end){
+		this.start = start;
+		this.end = end;
+	}
+}
