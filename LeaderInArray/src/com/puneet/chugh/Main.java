a9c66f5d304828b12269d14ef6e32704b5f9e88f
Puneet Chugh <chughpuneet2005@gmail.com>
2019-05-14 22:20:21 -0600

    another way of calculating leaders

--- LeaderInArray/src/com/puneet/chugh/Main.java
+++ LeaderInArray/src/com/puneet/chugh/Main.java
@@ -18,10 +18,31 @@ public class Main{
 		return list;
 	}
 
+	//another way
+	public static List<Integer> leaders1(int[] array){
+
+		List<Integer> list = new LinkedList();
+		list.add(array[array.length-1]);
+		int max = array[array.length-1];
+		for(int counter=array.length-2; counter>=0; counter--){
+			if(max < array[counter]){
+				max = array[counter];
+				list.add(array[counter]);
+			}
+		}
+		return list;
+	}
+
 	public static void main(String[] args){
 
-		System.out.printf("The leaders are :");
+		System.out.printf("The leaders are : ");
 		for(int oneInt : leaders(new int[]{16, 17, 4, 3, 5, 2})){
+			System.out.printf("%d\t", oneInt);
+		}
+		
+		System.out.println("");
+		System.out.printf("The leaders are : ");
+		for(int oneInt : leaders1(new int[]{16, 17, 4, 3, 5, 2})){
 			System.out.printf("%d\t", oneInt);
 		}
 	}
